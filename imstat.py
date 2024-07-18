c5ec09c26bf55d94ae6f76c202dab1f6102ecc18
Akimasa Nishida <akimasa.nishida@grad.nao.ac.jp>
2024-07-18 18:05:34 +0900

    Update imstat

--- imstat.py
+++ imstat.py
@@ -1,18 +1,34 @@
 import numpy as np
 from .Image import Image
 
-def imstat(imagename: str, region_mask: str, inverse_mask: bool = False):
+def imstat(imagename: str, width: int=None, height: int=None, region_mask: str=None, inverse_mask: bool=False):
     """
     ToDo: Now only rms can be calculated. Add all quantities of `imstat`.
     """
-    img = Image(imagename)
-    img_mask = Image(region_mask)
-    max_mask = np.max(img_mask.img)
-    min_mask = np.min(img_mask.img)
-    img_mask.img = (img_mask.img - min_mask) / (max_mask - min_mask)
-    if inverse_mask:
-        img_mask.img = 1 - img_mask.img
-    n_pix = np.sum(img_mask.img)
-    img.img *= img_mask.img
-    return np.sqrt(np.sum(np.square(img.img)) / n_pix)
+    ret = {}
+    img = Image(imagename, width, height)
+    width, height = img.get_fig_size()
+    ret['unit'] = img.im_unit
+    # process mask
+    data = []
+    if region_mask != None:
+        img_mask = Image(region_mask)
+        max_mask = np.max(img_mask.img)
+        min_mask = np.min(img_mask.img)
+        img_mask.img = (img_mask.img - min_mask) / (max_mask - min_mask)
+        if inverse_mask:
+            img_mask.img = 1 - img_mask.img
+        for i in range(height):
+            for j in range(width):
+                if img_mask.img[i][j] == 1:
+                    data.append(img.img[i][j])
+    else:
+        for i in range(height):
+            for j in range(width):
+                data.append(img.img[i][j])
+    data = np.array(data)
+    ret['rms'] = np.sqrt(np.sum(np.square(data)) / data.size)
+    ret['max'] = np.max(data)
+    ret['min'] = np.min(data)
+    return ret
 
